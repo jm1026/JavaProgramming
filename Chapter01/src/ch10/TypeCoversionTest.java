80b5436a1cd2c29535d787984fe9e063a7873fb0
jm1026 <a50461118@gmail.com>
2022-02-24 12:32:09 +0900

    22.02.24

--- Chapter01/src/ch10/TypeCoversionTest.java
+++ Chapter01/src/ch10/TypeCoversionTest.java
@@ -4,9 +4,8 @@ public class TypeCoversionTest {
 
 	public static void main(String[] args) {
 		
-		
 		int iNum = 255;
-		byte bnum = (byte)iNum;
+		byte bnum = (byte)iNum; // 데이터 손실로 엉뚱한 수가 반환
 		
 		System.out.println(bnum);
 		
@@ -14,13 +13,12 @@ public class TypeCoversionTest {
 		int inum = (int)dnum;
 		
 		System.out.println(inum);
-		
-		
+				
 		double dnum2 = 1.2;
 		float fnum2 = 0.9F;
 		
-		int iNum1 = (int)dnum2 + (int)fnum2;
-		int iNum2 = (int)(dnum2 + fnum2);
+		int iNum1 = (int)dnum2 + (int)fnum2; // 1 + 0
+		int iNum2 = (int)(dnum2 + fnum2);    // (int)2.1
 		
 		System.out.println(iNum1);
 		System.out.println(iNum2);
