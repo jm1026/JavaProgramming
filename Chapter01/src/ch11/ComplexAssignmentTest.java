68df6dbe9d9ecfcaaa04bb21164eafdaa893799f
jm1026 <a50461118@gmail.com>
2022-02-24 12:30:14 +0900

    22.03.24

---
+++ Chapter01/src/ch11/ComplexAssignmentTest.java
@@ -0,0 +1,15 @@
+package ch11;
+
+public class ComplexAssignmentTest {
+
+	public static void main(String[] args) {
+		
+		int myNum = 10;
+		int yourNum = 20;
+		
+		myNum += yourNum;
+		System.out.println(myNum);
+
+	}
+
+}
