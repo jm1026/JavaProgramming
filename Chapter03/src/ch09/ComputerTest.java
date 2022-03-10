3a7e53273297725610ada6adecb2ff163efbd0ee
jm1026 <a50461118@gmail.com>
2022-03-10 15:45:36 +0900

    22.03.10

---
+++ Chapter03/src/ch09/ComputerTest.java
@@ -0,0 +1,13 @@
+package ch09;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+		Computer computer = new Desktop();
+		computer.display();
+		computer.turnOff();
+		
+		NoteBook myNote = new MyNoteBook();
+	}
+
+}
