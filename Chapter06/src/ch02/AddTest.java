364ba673982b4225ebc007aa5313490554df7c8a
jm1026 <a50461118@gmail.com>
2022-03-21 20:04:46 +0900

    22.03.21

---
+++ Chapter06/src/ch02/AddTest.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class AddTest {
+
+	public static void main(String[] args) {
+		
+		Add addl = (x, y)->{return x+y;};
+		
+		System.out.println(addl.add(2, 3));
+		
+		
+		
+		
+	}
+
+}
