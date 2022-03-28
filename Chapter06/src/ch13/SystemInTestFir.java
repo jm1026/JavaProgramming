3837c3f0d5471315c2584d906468db58e8d14371
jm1026 <a50461118@gmail.com>
2022-03-28 18:10:10 +0900

    22.03.24

---
+++ Chapter06/src/ch13/SystemInTestFir.java
@@ -0,0 +1,20 @@
+package ch13;
+
+import java.io.IOException;
+
+public class SystemInTestFir {
+
+	public static void main(String[] args) {
+
+		System.out.println("알파벳 하나를 쓰고 [Enter]를 누르세요");
+		
+		int i;
+		try {
+			i = System.in.read();
+			System.out.println(i);
+			System.out.println((char)i);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+}
